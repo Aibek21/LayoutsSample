bc29d0625c18e15a7709b99b265e0d5ac72a9538
Aibek21 <aibekkuralbaev@gmail.com>
2017-08-29 09:13:58 +0600

    Update

--- app/src/main/java/kz/kbtu/layoutssample/Main2Activity.java
+++ app/src/main/java/kz/kbtu/layoutssample/Main2Activity.java
@@ -3,6 +3,8 @@ package kz.kbtu.layoutssample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -12,8 +14,27 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = (TextView) findViewById(R.id.text1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
+
+    public void buttonClicked(View v){
+
+        switch (v.getId()){
+            case R.id.text1:
+
+                break;
+        }
+
+    }
 
 }
